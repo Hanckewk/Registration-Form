a766edf6a71b6f86ef11915fb23f4082ce9ff91a
Hancke Koegelenberg <hancke006@gmail.com>
2024-01-14 16:00:09 +0200

    Add files via upload

---
+++ Registration Form.py
@@ -0,0 +1,11 @@
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import Boxlayout
+
+class RegistrationApp(App):
+    def (self):
+        self.title = "Registration Form"
+        layout = Boxlayout(orientation = 'vertical', badding = 30, spacing=10)
+
+        return layout
+
